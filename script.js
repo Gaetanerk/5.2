d27c70530112cb94059a430e51c683e9882b3a74
Gaetanerk <gaetan.turben@outlook.com>
2022-04-26 11:22:39 +0200

    retrait if

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let response = prompt("donne moi un chiffre compris entre 10 et 20");
 while (response < 10 || response > 20) {
   if (response < 10) {
     response = prompt("Plus grand");
-  } else if (response > 20) {
+  } else (response > 20) {
     response = prompt("Plus petit");
   }
 }
